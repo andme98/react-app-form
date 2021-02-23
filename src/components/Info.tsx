009857788e74a1dc9072e20f6eb2267d726cf3d5
Andres <andresmeregildo@gmail.com>
2021-02-23 16:49:05 -0500

    adding some info to the page...

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -5,9 +5,13 @@ import '../index.css';
 function Info() {
   return (
     <>
+      <h3>Título</h3>
       <p>Este era un texto descriptivo para la página web en React, o eso intentaba ser.</p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
       <img src="../gataloca.jfif" alt="No se pudo cargar la imagen"></img>
-    
+
     </>
   );
 }
